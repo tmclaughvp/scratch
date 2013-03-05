ba0d344f0f8d2f1ab4a0695be26ef4023b6b70d4
Tom McLaughlin <tmclaughlin@vistaprint.com>
2013-03-05 15:16:18 -0500

    Add baseHTTPServer script.

---
+++ misc/baseHTTPServer.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+import BaseHTTPServer
+
+server_host = 'localhost'
+server_port = 80
+
+class MyHandler(BaseHTTPServer.BaseHTTPRequestHandler):
+    def do_HEAD(s):
+        s.send_response(200)
+        s.send_header("Content-type", "text/html")
+        s.end_headers()
+    def do_GET(s):
+        """Respond to a GET request."""
+        s.send_response(200)
+        s.send_header("Content-type", "text/html")
+        s.end_headers()
+        s.wfile.write("<html><head><title>Title goes here.</title></head>")
+        s.wfile.write("<body><p>This is a test.</p>")
+        # If someone went to "http://something.somewhere.net/foo/bar/",
+        # then s.path equals "/foo/bar/".
+        s.wfile.write("<p>You accessed path: %s</p>" % s.path)
+        s.wfile.write("</body></html>")
+
+if __name__ == '__main__':
+    server_class = BaseHTTPServer.HTTPServer
+    httpd = server_class((server_host, server_port), MyHandler)
+    try:
+        httpd.serve_forever()
+    except KeyboardInterrupt:
+        pass
+    httpd.server_close()
